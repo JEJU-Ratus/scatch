b44aeb42e7a2ccf4379df2b391079cfafcf7510a
JEJU-Ratus <smh1141@naver.com>
2023-02-24 17:28:18 +0900

    test

--- script/login.js
+++ script/login.js
@@ -1,38 +1,46 @@
 function loginForm_check() { // 유효성 검사. ID와
   const uid = document.getElementById('uid'); // 사용자 아이디 파라미터 연동
   const pwd = document.getElementById('pwd'); // 비밀번호 파라미터 연동
-  
+
   //경고메시지
   const idBlank = document.getElementById('idBlank');
   const pwBlank = document.getElementById('pwBlank');
-  const idReg = document.getElementById('idReg');
+  const idpwReg = document.getElementById('idpwReg');
 
   //유효성을이용해 로그인 아이디 틀린것처럼 만들기
   const REG_UID = /^[a-z0-9]{1}[a-z0-9_-]{4,20}$/; // user ID 규칙
+  const REG_PWD = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{9,20}$/;//비밀번호 규칙
 
   let checkAll = true; // 유효성 검사 결과 최종 값이 들어갈 곳. 기본값 true
 
-  if (!uid.value){
+  if (!uid.value) {
     idBlank.classList.remove('wrap_fold');
     pwBlank.classList.add('wrap_fold');
-    idReg.classList.add('wrap_fold');
-      uid.focus();
-      checkAll = false;
+    idpwReg.classList.add('wrap_fold');
+    uid.focus();
+    checkAll = false;
   }
-  else if (!pwd.value){
+  else if (!pwd.value) {
     idBlank.classList.add('wrap_fold');
     pwBlank.classList.remove('wrap_fold');
-    idReg.classList.add('wrap_fold');
-      pwd.focus();
-      checkAll = false;
+    idpwReg.classList.add('wrap_fold');
+    pwd.focus();
+    checkAll = false;
   }
-  else if(!REG_UID.test(uid.value)){
+  else if (!REG_UID.test(uid.value)) {
     idBlank.classList.add('wrap_fold');
     pwBlank.classList.add('wrap_fold');
-    idReg.classList.remove('wrap_fold');
-      uid.focus();
-      checkAll = false;
-    }
+    idpwReg.classList.remove('wrap_fold');
+    uid.focus();
+    checkAll = false;
+  }
+  else if (!REG_PWD.test(pwd.value)) {
+    idBlank.classList.add('wrap_fold');
+    pwBlank.classList.add('wrap_fold');
+    idpwReg.classList.remove('wrap_fold');
+    uid.focus();
+    checkAll = false;
+  }
 
   if (!checkAll) {
     return false;
---
+++ script/market.js
@@ -0,0 +1,10 @@
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const postDate = document.querySelectorAll('.postDate');
+
+for(i=0; i< postDate.length;i++){
+  postDate[i].innerHTML = year + "." + month + "." + day;
+}
+
