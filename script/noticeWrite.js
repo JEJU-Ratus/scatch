6eeb13f88731f540e4450fc85deedce63847e509
JEJU-Ratus <smh1141@naver.com>
2023-02-28 15:18:51 +0900

    사이트 공지사항 구현

--- script/noticeWrite.js
+++ script/noticeWrite.js
@@ -1,15 +1,24 @@
 let oEditors = []
 
-    smartEditor = function() {
-      console.log("Naver SmartEditor")
-      nhn.husky.EZCreator.createInIFrame({
-        oAppRef: oEditors,
-        elPlaceHolder: "editorTxt",
-        sSkinURI: "smarteditor/SmartEditor2Skin.html",
-        fCreator: "createSEditor2"
-      })
-    }
+smartEditor = function () {
+  console.log("Naver SmartEditor")
+  nhn.husky.EZCreator.createInIFrame({
+    oAppRef: oEditors,
+    elPlaceHolder: "editorTxt",
+    sSkinURI: "smarteditor/SmartEditor2Skin.html",
+    fCreator: "createSEditor2"
+  })
+}
 
-    $(document).ready(function() {
-      smartEditor()
-    })
+$(document).ready(function () {
+  smartEditor()
+})
+
+
+const submit = document.getElementById('submit');
+
+
+submit.addEventListener('click',function(){
+  console.log("click");
+  document.getElementById('btn_submit').click();
+})
