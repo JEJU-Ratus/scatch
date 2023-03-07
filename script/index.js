ce7325886cf3c34c4f72f2ab3826603a972bf2ae
JEJU-Ratus <smh1141@naver.com>
2023-03-07 17:02:18 +0900

    메인페이지 완성

--- script/index.js
+++ script/index.js
@@ -1,32 +1,51 @@
 const btn_slideLeft = document.querySelector('.btn_slideLeft'),// 왼쪽 화살표 버튼
-btn_slideRight = document.querySelector('.btn_slideRight'),// 오른쪽 화살표 버튼
-cont_mainSlide = document.querySelector('.cont_mainSlide'),// 메인페이지의 카드 슬라이드.
-allImg = 4; // 모든 이미지의 수
+  btn_slideRight = document.querySelector('.btn_slideRight'),// 오른쪽 화살표 버튼
+  cont_mainSlide = document.querySelector('.cont_mainSlide'),// 메인페이지의 카드 슬라이드.
+  playPause = document.getElementsByClassName('playPause'), // 슬라이드 on/off 버튼
+  allImg = 4; // 모든 이미지의 수
 
-let countImg = 0; // 현재 노출 이미지 번호
-let timer = setInterval(auto_slide,8000); // 슬라이드 자동.
+let countImg = 0, // 현재 노출 이미지 번호
+  timer = setInterval(auto_slide, 8000), // 슬라이드 자동.
+  powerOnOff = 1;
+
+  //재생 및 정지 버튼 조작
+  for(let i=0;i<playPause.length;i++){
+    playPause[i].addEventListener('click',function(){
+      if(powerOnOff > 0){ // powerOnOff 가 1일때 재생 0일때 정지
+        clearInterval(timer); //정지
+        playPause[i].src = "images/play.png";
+        powerOnOff--;
+      }
+      else{
+        timer = setInterval(auto_slide, 8000) // 재생
+        playPause[i].src = "images/pause.png";
+        powerOnOff++
+      }
+      
+    })
+  }
 
 
 // 오른쪽 화살표 클릭
-btn_slideRight.addEventListener('click',function(){
-  if(countImg<allImg-1){
+btn_slideRight.addEventListener('click', function () {
+  if (countImg < allImg - 1) {
     countImg++;
     adjust_countImg();
   }
-  else{
-    countImg=0;
+  else {
+    countImg = 0;
     adjust_countImg();
   }
 });
 
 
 // 왼쪽 화살표 클릭
-btn_slideLeft.addEventListener('click',function(){
-  if(countImg > 0){
+btn_slideLeft.addEventListener('click', function () {
+  if (countImg > 0) {
     countImg--;
     adjust_countImg();
   }
-  else{
+  else {
     countImg = 3;
     adjust_countImg();
   }
@@ -34,19 +53,20 @@ btn_slideLeft.addEventListener('click',function(){
 
 
 // 현재 노출 이미지 번호 조정 함수
-function adjust_countImg(){
-  cont_mainSlide.style.transform = `translate(${countImg*(-100)}vw)`;
-    clearInterval(timer);
-    timer = setInterval(auto_slide,8000);
+function adjust_countImg() {
+  cont_mainSlide.style.transform = `translate(${countImg * (-100)}vw)`;
+  clearInterval(timer);
+  timer = setInterval(auto_slide, 8000);
 }
 
-function auto_slide(){
-  if(countImg<allImg-1){
+function auto_slide() {
+  if (countImg < allImg - 1) {
     countImg++;
-    cont_mainSlide.style.transform = `translate(${countImg*(-100)}vw)`;
+    cont_mainSlide.style.transform = `translate(${countImg * (-100)}vw)`;
   }
-  else{
+  else {
     countImg = 0;
-    cont_mainSlide.style.transform = `translate(${countImg*(-100)}vw)`;
+    cont_mainSlide.style.transform = `translate(${countImg * (-100)}vw)`;
   }
 }
+
